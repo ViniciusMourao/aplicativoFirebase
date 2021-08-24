b5ff9c73e22dcc3c8c091e505852a0a51d9fff3d
ViniciusMourao <vinivinicius56@outlook.com>
2021-08-24 08:48:31 -0300

    primeiro upload do projeto

---
+++ www/js/firebase.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBapSXXseI1el6nZEll0_UOVdeDiwa3qi8",
+    authDomain: "app-agendamentos-346df.firebaseapp.com",
+    projectId: "app-agendamentos-346df",
+    storageBucket: "app-agendamentos-346df.appspot.com",
+    messagingSenderId: "199958608993",
+    appId: "1:199958608993:web:e76b68d5d57f27c15afe20",
+    measurementId: "G-X42MKFX59X"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
